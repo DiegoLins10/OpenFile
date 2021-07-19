246f2bf7718ae1717afbcb3c9139ec1d1daa9fef
diegolins10 <diegofernandeslins@gmail.com>
2021-07-19 20:21:29 -0300

    Add project files.

---
+++ OpenFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace OpenFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(@"C:\Users\DiegoLins\Desktop\FONE.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+        }
+    }
+}
